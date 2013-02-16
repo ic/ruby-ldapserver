848b2951514f7265a35d2ba7900ae87256766354
Eric Platon <zaraki@gmx.com>
2013-02-16 16:59:26 +0900

    Added a few basic tests for Filter.

--- spec/filter_spec.rb
+++ spec/filter_spec.rb
@@ -2,9 +2,33 @@ require 'ldap/server/filter'
 
 describe LDAP::Server::Filter do
 
+  #
+  # Filters are executed with the ::run method, so
+  #   the subject must be the class, not an instance.
+  #
   subject { LDAP::Server::Filter }
 
+  let :input do
+    {
+      'foo' => [ 'abc', 'def' ],
+      'bar' => [ 'wibblespong' ],
+    }
+  end
+
   context 'Simple filters' do
+
+    it 'should '
+
+  end
+
+  context 'Bad filters' do
+
+    it 'raises an operation error for unknown filter types' do
+      expect {
+        subject.run([:whatever], {})
+      }.to raise_error(LDAP::ResultError::OperationsError)
+    end
+
   end
 
   context 'Edge-case filters' do
@@ -15,6 +39,10 @@ describe LDAP::Server::Filter do
 
     it 'returns false for a "false" filter' do
       subject.run([:false], {}).should be_false
+    end
+
+    it 'returns nil for a "undef" filter' do
+      subject.run([:undef], {}).should be_nil
     end
 
   end
